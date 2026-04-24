21dfacd000497a30fb20eb03f3349c1c5635b35d
dandannn <dandansandan04@gmail.com>
2026-04-24 19:17:07 +0200

    LED test correcte code

--- LED_Test.c
+++ LED_Test.c
@@ -1,72 +1,80 @@
 /*
  * File:   LED_Test.c
  * Author: Danie
- *
- * Created on April 22, 2026, 1:27 PM
+ * Modified for: ATmega32U4 using provided iom32u4.h definitions
  */
 
 #include <avr/io.h>
+#include "USB_header_1.h"
 #include <stdint.h>
-#include "USB_header.h"
+#include <util/delay.h>
 
-#define LED1_PIN    PD5
-#define LED3_PIN    PD0 
-#define LED_DDR     DDRD
-#define LED_PORT    PORTD
+// Definities op basis van de iom32u4.h namen
+#define LED1_BIT    PIND5  // Bit 5 in PORTD/DDRD
+#define LED3_BIT    PIND0  // Bit 0 in PORTD/DDRD
+#define LED_DDR    DDRD    // Data Direction Register D
+#define LED_PORT  PORTD   // Port Register D
+//#define DELAY 3000000UL
 
-// Bij 16MHz zorgt 1.000.000 loops voor ongeveer 0,5 tot 1 seconde vertraging.
-void manual_delay(uint32_t loops) {
+/*void manual_delay(uint32_t loops) {
     for (volatile uint32_t i = 0; i < loops; i++) {
         __asm__("nop"); 
     }
-}
+}*/
 
 void setupPLL(void) {
-    PLLCSR = (1 << PINDIV);
-    PLLCSR |= (1 << PLLE); 
+    // Gebruik de namen uit de header: PLLCSR en de bits PINDIV, PLLE, PLOCK
+    PLLCSR = (1 << PINDIV); 
+    PLLCSR |= (1 << PLLE);  // Start PLL
     
-   
-    // PDIV2: 16MHz -> 48MHz
-    PLLFRQ |= (1 << PDIV2);
-    
+    // Wacht tot PLL stabiel is (Lock bit)
     while(!(PLLCSR & (1 << PLOCK))); 
+    
+    // Gebruik PLLFRQ en bit PDIV2 (voor 48MHz bij 8MHz input)
+    PLLFRQ = (1 << PDIV2); 
 }
 
 void initUSB(void) {
-    USBCON |= (1 << USBE);
-    USBCON &= ~(1 << FRZCLK);
+    // Gebruik USBCON en bits USBE, FRZCLK, OTGPADE
+    USBCON |= (1 << USBE);    // Enable USB controller
+    USBCON &= ~(1 << FRZCLK); // Ontvries USB klok
     
     setupPLL();
     
-    USBCON |= (1 << OTGPADE); 
-    UDCON &= ~(1 << DETACH); // Verbind met USB host
+    USBCON |= (1 << OTGPADE); // Enable VBUS pad
+    
+    // Gebruik UDCON en bit DETACH
+    UDCON &= ~(1 << DETACH);  // Re-attach USB (zorgt dat de PC hem ziet)
 }
 
 void setupHardware(void) {
-    LED_DDR |= (1 << LED1_PIN) | (1 << LED2_PIN);
+    // Gebruik de DDR bit definities (DDD0, DDD5 etc. zijn ook gedefinieerd als 0 en 5)
+    LED_DDR |= (1 << DDB5) | (1 << DDB0); // Let op: in DDRD heten ze DDD5/DDD0 in de header
+    // Correctie naar de specifieke header namen voor DDRD:
+    DDRD |= (1 << DDD5) | (1 << DDD0);
     
     // LEDs uit bij start
-    LED_PORT &= ~((1 << LED1_PIN) | (1 << LED2_PIN));
+    PORTD &= ~((1 << PORTD5) | (1 << PORTD0));
 }
 
 int main(void) {
+    //manual_delay(DELAY); 
+    _delay_ms(3000);
     setupHardware();
     initUSB();
 
-    // Debugging loop: Als dit werkt, draait je code en werkt je kristal/klok.
     while(1) {
-        // Toggle LED 1
-        LED_PORT |= (1 << LED1_PIN);
-        LED_PORT &= ~(1 << LED2_PIN);
-        manual_delay(1000000);
+        // Toggle LED 1 aan, LED 3 uit
+        PORTD |= (1 << PORTD5);
+        PORTD &= ~(1 << PORTD0);
+        //manual_delay(DELAY);
+        _delay_ms(3000);
 
-        // Toggle LED 2
-        LED_PORT &= ~(1 << LED1_PIN);
-        LED_PORT |= (1 << LED2_PIN);
-        manual_delay(1000000);
-        
-        // Hier kun je later de USB taken aanroepen:
-        // usb_poll_enumeration();
+        // Toggle LED 1 uit, LED 3 aan
+        PORTD &= ~(1 << PORTD5);
+        PORTD |= (1 << PORTD0);
+        //manual_delay(DELAY);
+        _delay_ms(3000);
     }
 
     return 0;
